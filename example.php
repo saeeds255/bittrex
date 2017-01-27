ac3def346c7fe6f6e8a4163205846413947d60ef
saeeds255 <upse@yahoo.com>
2017-01-28 01:44:39 +0330

    Update example.php

--- example.php
+++ example.php
@@ -8,23 +8,26 @@ massbuy.php:
 	$key = '4bec433f95e54562aeeefae92ebedb84';
 	$secret = '6171690af7364ea2a951dc85d00e1130';
 
-	$b = new Client ($key, $secret);
-	$result = array();
-	$markets = array();
-	$marketsummary = array();
-	//var_dump ($b->buyLimit ('BTC-DAR', 10.9, 0.00005488));
-	var_export ($b->getMarkets ());
-	$markets = $b->getMarkets ();
-	foreach ($markets as $item) {
-    		$marketname = $item->MarketName;
-		$currency = $item->MarketCurrency;
-    		$marketsummary = $b->getMarketSummary ($marketname);
-		$volume = $marketsummary[0]->BaseVolume;
-		$ask = $marketsummary[0]->Ask;
-		if($volume >= 0.2 && $ask>= 0.0000003 && $currency != "BTC"){
-			$buyqua = 0.0006/$ask;
-			echo $buyqua." ".$currency." "."with price of"." ".$ask." "."bought. volume= ".$volume."\n";
-			$b->buyLimit ($marketname, $buyqua, $ask);
+        $b = new Client ($key, $secret);
+        $balance = array();
+        $markets = array();
+        $marketsummary = array();
+//      var_export ($b->getMarkets ());
+        $markets = $b->getMarkets ();
+        foreach ($markets as $item) {
+                $marketname = $item->MarketName;
+                $currency = $item->MarketCurrency;
+                $isactive = $item->IsActive;
+                $marketsummary = $b->getMarketSummary ($marketname);
+                $volume = $marketsummary[0]->BaseVolume;
+                $ask = $marketsummary[0]->Ask;
+                $balance = $b->getBalance ($currency);
+                $mybalance = $balance->Available;
+                if($isactive && $mybalance == 0 && $volume >= 1 && $ask>= 0.0000003 && $currency != "BTC"){
+                        $buyqua = 0.0006/$ask;
+                        echo $buyqua." ".$currency." "."with price of"." ".$ask." "."bought. volume= ".$volume."\n";
+                        $b->buyLimit ($marketname, $buyqua, $ask);
+                        sleep(1);
 		}
 	}
 	//echo $ask;
