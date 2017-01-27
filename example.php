89a204a74d14c68d0589e49eda57a17bfe35e1c8
saeeds255 <upse@yahoo.com>
2017-01-28 01:02:58 +0330

    Update example.php

--- example.php
+++ example.php
@@ -24,9 +24,42 @@
 			$b->buyLimit ($marketname, $buyqua, $ask);
 		}
 	}
-	echo $ask;
+	//echo $ask;
         //var_export ($b->getMarketSummary ('BTC-DAR'));
         //$result = $b->getMarketSummary('BTC-DAR');
         //$ask = $result[0]->Ask;
 	
 	echo "\n\n";
+
+
+
+
+
+<?php
+
+	require __DIR__.'/src/edsonmedina/bittrex/Client.php';
+
+	use edsonmedina\bittrex\Client;
+
+	$key = '4bec433f95e54562aeeefae92ebedb84';
+	$secret = '6171690af7364ea2a951dc85d00e1130';
+
+        $b = new Client ($key, $secret);
+        $balance = array();
+        $markets = array();
+        $marketsummary = array();
+        $markets = $b->getMarkets ();
+        foreach ($markets as $item) {
+                $marketname = $item->MarketName;
+                $currency = $item->MarketCurrency;
+                $marketsummary = $b->getMarketSummary ($marketname);
+                $bid = $marketsummary[0]->Bid;
+                $balance = $b->getBalance ($currency);
+                $mybalance = $balance->Available;
+                if($mybalance != 0){
+                        $b->sellLimit ($marketname, $mybalance, $bid);
+                        echo $mybalance." ".$currency." "."with price of"." ".$bid." "."sold\n";
+                        sleep(1);
+                }
+        }
+        echo "\n\n";
